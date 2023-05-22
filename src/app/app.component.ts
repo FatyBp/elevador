5600098b74b4242bca790920b7edcf41caf794ad
Fatybp <fatimabenitez796@gmail.com>
2023-05-21 20:38:50 -0600

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {
-  Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,27 +7,32 @@ import {
 })
 export class AppComponent {
   currentFloor = 1;
-  maxWeight = 700;
-  minWeinght = 0;
-  acumulador = 0;
+  maxWeight = 1000;
+  maxPeople = 10;
+  weight=0;
+  people=0;
+  Asc(){
+    this.weight += parseInt((<HTMLInputElement>document.getElementById('weight')).value);
+    this.people += parseInt((<HTMLInputElement>document.getElementById('people')).value);
+    console.log(this.weight)
+    console.log(this.people)
+
+  }
+  Desc(){
+    this.weight -= parseInt((<HTMLInputElement>document.getElementById('weight')).value);
+    this.people -= parseInt((<HTMLInputElement>document.getElementById('people')).value);
+    console.log(this.people)
+    console.log(this.weight)
+  }
 
   goToFloor() {
-    const floor = parseInt(( < HTMLInputElement > document.getElementById('floor')).value);
-    const weight = parseInt(( < HTMLInputElement > document.getElementById('weight')).value);
-
-    // if (weight==0) {
-    //   for (let index = 0; index < floor; index++) {
-    //     alert('piso ' +  (index)) 
-    //   }
-    // }if (floor >= 1 && floor <= 5 && weight >= 0 && (weight*70) <= this.maxWeight) {
-    //   for (let index = 0; index < floor; index++) {
-    //     alert('piso ' +  (index+1)) 
-    //   }
-    // } else if (weight==700) {
-    //   for (let index = 0; index < floor; index++) {
-    //     alert('piso ' +  (index-1)) 
-
-    // }
-    // }
+    const floor = parseInt((<HTMLInputElement>document.getElementById('floor')).value);
+    if (floor >= 1 && floor <= 5 && this.weight >= 0 && this.weight <= this.maxWeight && this.people >= 0 && this.people <= this.maxPeople) {
+      alert(`estas en el piso ${this.currentFloor}`);
+      alert(`Ir al piso ${floor}`);
+      this.currentFloor=floor;
+    } else {
+      alert('Ha alcanzado el maximo del limite en personas o en peso');
+    }
   }
 }
